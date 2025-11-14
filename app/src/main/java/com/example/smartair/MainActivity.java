47d90f5485173ba88e4774d7e8acaaa807481774
Jiayi Qi <qijiayi28@gmail.com>
2025-11-14 18:25:03 -0500

    Connect Firestore and add test Button 1,2

--- app/src/main/java/com/example/smartair/MainActivity.java
+++ app/src/main/java/com/example/smartair/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.smartair;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btn1 = findViewById(R.id.btnTest1);
+        Button btn2 = findViewById(R.id.btnTest2);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance().collection("testLogs").getFirestore();
+
+        btn1.setOnClickListener(v -> {
+
+            Map<String, Object> data = new HashMap<>();
+            data.put("type", "Rescue");
+            data.put("time", "now");
+            db.collection("testLogs").add(data);
+
+        });
+
+        btn2.setOnClickListener(v -> {
+            Map<String, Object> data = new HashMap<>();
+            data.put("type", "PEF");
+            data.put("value", 300);
+            db.collection("testLogs").add(data);
+        });
+
+
     }
 }
